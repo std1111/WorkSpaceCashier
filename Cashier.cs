2966f52fa71a0621c28c00c836e15ea79a1611d4
gennady.matyuk <std1111@gmail.com>
2021-07-03 17:40:22 +0300

    44

---
+++ Cashier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSpaceCashier
+{
+    class Cashier
+    {
+        private string login;
+        private string password;
+
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
